9173805ebb60af7afcd440a18fe02740e0ca88fc
Shaun <sayedshaun4@gmail.com>
2023-11-18 11:02:51 +0600

    Version 1

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# Steam Review Analysis
- 
+# Steam Game Review Classification
+
+This project focuses on classifying Steam game reviews using various machine learning models to predict sentiment analysis.
+
+## Overview
+
+The aim of this project is to develop a classification system for Steam game reviews. The project utilizes a dataset (`steam_reviews.csv`) containing textual reviews along with corresponding sentiment labels. The models implemented in this project include:
+- Decision Tree Classifier (`DecisionTreeClassifier.joblib`)
+- Logistic Regression Classifier (`LogisticRegression.joblib`)
+- Multinomial Naive Bayes Classifier (`MultinomialNB.joblib`)
+- Random Forest Classifier (`RandomForestClassifier.joblib`)
+- Support Vector Machine Classifier (`SVM.joblib`)
+- XGBoost Classifier (`XGBClassifier.joblib`)
+
+## File Structure
+
+- **.streamlit**: Configuration for theming in the Streamlit app.
+- **Images**: Images related to the project or for use within the app.
+- **Models**: Contains pre-trained models and a notebook (`Notebook.ipynb`) detailing model training and evaluation.
+- **app.py**: The Streamlit web application for interacting with the models and conducting predictions.
+- **requirements.txt**: Necessary dependencies and packages required to run the project.
+- **steam_reviews.csv**: Dataset containing Steam game reviews for training and testing.
+
+## Usage
+
+### Installation
+
+1. Clone the repository:
+   ```bash
+   [git clone https://github.com/your-username/steam-game-review-classification.git](https://github.com/SayedShaun/Steam-Game-Review-Classification.git)
+   ```
+
+2. Navigate to the project directory:
+   ```bash
+   cd steam-game-review-classification
+   ```
+
+3. Install the required dependencies:
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+### Running the Application
+
+Run the Streamlit app:
+   ```bash
+   streamlit run app.py
+   ```
+
+### Model Usage
+
+The pre-trained models are available in the `Models` directory. To use them in your own scripts:
+
+1. Load the desired model using a library like `joblib`.
+2. Utilize the model's `predict` method to classify new Steam game reviews.
+
+## Contribution
+
+Contributions to enhance the models, add new features, or improve the app are welcome! Fork the repository, create a new branch, make your changes, and submit a pull request.
+
+---
+Feel free to personalize this README by adding or modifying sections based on your specific project details or additional information you want to include.
---
+++ app.py
@@ -0,0 +1,83 @@
+import streamlit as st
+import re
+import xgboost
+import nltk
+from nltk.stem.porter import PorterStemmer
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+st.title("Steam Game Review Clasification")
+input_data = st.text_area("Leave Steam Review", height=150)
+def preprocess(text):
+    stopwords = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from',
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're',
+             's', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those',
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+    
+    cleaned_text = re.sub(r"http\S+", "", text)
+    cleaned_text = re.sub(r"<[^>]*>", "", cleaned_text)
+    cleaned_text = re.sub(r"[^\w\s]", "", cleaned_text)
+    cleaned_text = re.sub(r"(.)\1{2,}", r"\1", cleaned_text)
+    cleaned_text = re.sub(r"\d", "", cleaned_text)
+    cleaned_text = " ".join([word for word in cleaned_text.split() if word.lower() not in stopwords])
+
+    ps = PorterStemmer()
+    stemmed_words = [ps.stem(word) for word in cleaned_text.split()]
+    cleaned_text = " ".join(stemmed_words)
+
+    return cleaned_text
+    
+
+def text_transform(text):
+    tfidf = joblib.load("Models/tfidf")
+    vector = tfidf.transform([preprocess(text)])
+    return vector
+vector = text_transform(input_data)
+
+def prediction(text):
+    xgb = joblib.load("Models/XGBClassifier.joblib")
+    lr = joblib.load("Models/LogisticRegression.joblib")
+    mnb = joblib.load("Models/MultinomialNB.joblib")
+    dt = joblib.load("Models/DecisionTreeClassifier.joblib")
+    svm = joblib.load("Models/SVM.joblib")
+
+    naive_bayes = mnb.predict(text)
+    support_vector = svm.predict(text)
+    xgboost_model = xgb.predict(text)
+    decision_tree = dt.predict(text)
+    logistic_regression = lr.predict(text)
+    
+    predictions = [naive_bayes, support_vector, xgboost_model, decision_tree, logistic_regression]
+    predictions_array = np.array(predictions)
+    majority_predictions = np.round(np.mean(predictions_array, axis=0)).astype(int)
+    return majority_predictions
+
+output = prediction(vector)
+
+if st.button("Predict", use_container_width=True):
+    output = output
+    if output == 0:
+        st.image("Images/Not Recommended.gif")
+    elif output == 1:
+        st.image("Images/Recommended.gif")
